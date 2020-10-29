0f940c60c24696ec281f0036c59dd81a46c2d163
Damilare <Dmalomo@renmoney.com>
2020-10-29 18:27:30 +0100

    Test migrations deploy

---
+++ database/migrations/2020_10_29_172555_drop_tests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DropTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('tests');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+}
